7667b597cc10abf6daea3e8ca43ba4b11705f56b
Nate Thompson <nthompson55@gmail.com>
2015-09-17 20:06:19 -0500

    7 routing with multi views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,13 +1,16 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
   $http.get('phones/phones.json').success(function(data){
     $scope.phones = data
-    // $scope.phones = data.splice(0, 5);
   });
-  $scope.name = "World"
   $scope.orderProp = 'age'
 }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
