773f466cef3f32c145827da3c812d122bbc8b23e
Nate Thompson <nthompson55@gmail.com>
2015-09-17 20:36:21 -0500

    11 added service to handle querying json, this is really good for later changes to a real api

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,15 @@
 /* Controllers */
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
-  $http.get('phones/phones.json').success(function(data){
-    $scope.phones = data
-  });
+phonecatApp.controller('PhoneListCtrl', ['$scope', 'Phone', function ($scope, Phone) {
+  $scope.phones = Phone.query()
   $scope.orderProp = 'age'
 }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-    $http.get('phones/'+$routeParams.phoneId+'.json').success(function(data){
-      $scope.phone = data
-      $scope.mainImageUrl = data.images[0]
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId},function(phone){
+      $scope.mainImageUrl = phone.images[0]
     })
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
